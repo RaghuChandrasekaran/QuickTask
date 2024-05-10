57af05ad67f13688b31ca75c442fb5ddad17d68b
Raghu Chandrasekaran <raghu.9208@gmail.com>
2024-05-10 23:01:18 +0530

    :sparkles: Basic Layout of Quick Task (Todo List)

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../widgets/todotile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  List toDoList = [
+    ["my first task", false],
+    ["my second task", true],
+    ["my third task", false],
+    ["my fourth task", true],
+    ["my fifth task", false],
+  ];
+
+  void checkboxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Quick Task')),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkboxChanged(value, index),
+          );
+        },
+      ),
+    );
+  }
+}
